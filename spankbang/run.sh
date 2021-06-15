546e2fef1b6be3f930f6a715e39b49a4bf08604c
Zhāng Jūn <60116198+idolmatster@users.noreply.github.com>
2021-06-15 04:42:26 +0200

    Update run.sh

--- spankbang/run.sh
+++ spankbang/run.sh
@@ -2,9 +2,13 @@
 htmlfile="/tmp/index$(date +%s).html"
 curl "$1" -H 'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36 OPR/76.0.4017.177' -o "${htmlfile}"
 url=$(cat "${htmlfile}" | grep "contentUrl" | cut -d '"' -f 4)
+prefix="Watch "
+suffix=" Porn - SpankBang"
 filename1=$(cat "${htmlfile}" | pup 'title text{}')
+filename1=$(echo "$filename1" | sed -e "s/^$prefix//" -e "s/$suffix$//")
 tmp=$(echo "${url%\?*}")
 filename2=$(echo "${tmp##*/}")
 
+# echo $filename1 | cut -d '-' -f 2 | sed -e 's/^[[:space:]]*//'
 curl "${url}" -o "$filename1$filename2"
 rm "$htmlfile"
