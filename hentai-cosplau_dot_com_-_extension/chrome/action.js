509ff9ee86dfbfdcdae82afce2e3cc64d0eb3558
Zhāng Jūn <60116198+idolmatster@users.noreply.github.com>
2021-06-25 03:48:09 +0200

    Add files via upload

--- hentai-cosplau_dot_com_-_extension/chrome/action.js
+++ hentai-cosplau_dot_com_-_extension/chrome/action.js
@@ -1,25 +1,37 @@
 
-const downloadGallary = (imageUrl) => {
-    let title = document.title.split(" - ").slice(0, -2).join(' - ');
+const writeToClipboard = (str) => {
+    let el = document.createElement("textarea")
+    let success = false
+
+    el.value = str
+    document.body.append(el)
+    el.select()
+    success = document.execCommand("copy")
+    el.remove()
+
+    return success
+        ? Promise.resolve(str)
+        : Promise.reject(new Error("Unable to write to clipboard"))
+}
+
+const downloadGallary = (imageUrl, title) => {
     let tmp1 = imageUrl.split("/");
     let tmp2 = tmp1[tmp1.length - 1].split(".");
     let number = tmp2[0];
     let baseurl = tmp1.slice(0, -1).join('/');
 
-    for (let index = 1; index <= number; index++) {
-        console.log(`${baseurl}/${index}.jpg`);
-        chrome.downloads.download({
-            url: `${baseurl}/${index}.jpg`,
-            filename: `./${title}/${index}.jpg` // Optional
-        });
-    }
+    let gallery = { title, number, baseurl };
+    let gallery_json = JSON.stringify(gallery);
+
+    writeToClipboard(gallery_json);
 }
 
 const copyImageUrlToClipboard = (info, tab) => {
     let imageUrl = info.srcUrl;
+    let title = tab.title.split(" - ").slice(0, -2).join(' - ');
 
     if (imageUrl.includes("hentai-cosplays.com/"))
-        return downloadGallary(imageUrl);
+        return downloadGallary(imageUrl, title);
     else
         return alert('that\'s the the wrong website, it\'s not going to work here')
 }
