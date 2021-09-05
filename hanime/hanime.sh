c7f7ebf16697f751128e6e8eccf1cf4cbd0c085c
Zhāng Jūn <60116198+idolmatster@users.noreply.github.com>
2021-09-05 04:26:51 +0200

    fixed default 720p error

--- hanime/hanime.sh
+++ hanime/hanime.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 #download webpage
-page=$(curl $1)
+#page=$(curl $1)
+page=$(cat index.html)
 
 # content at the beginnig of and end of script tag
 data=$(echo ${page##*window.__NUXT__=})
@@ -11,12 +12,32 @@ data=$( echo ${data%%;</script>*})
 data2=$(echo $data | sed 's|\u002F|/|g')
 
 # for debug
-echo $data2 > tmp.json
+#echo $data2 > tmp.json
 
 # TODO : FIX choosing highest quality stream instead of 720p
 # parse the title and url with jq from script json of the page
-url=$(echo $data2 | jq '.state.data.video.videos_manifest.servers[0].streams[1].url' -r)
 title=$(echo $data2 | jq '.state.data.video.hentai_video.name' -r)
+
+videomanifest=$(echo $data2 | jq '.state.data.video.videos_manifest.servers[0]' -r)
+
+((resolution=0))
+url="empty"
+
+for k in  $(jq -r '.streams | keys | .[]' <<< "$videomanifest"); do
+    height=$( echo $videomanifest | jq ".streams[$k].height" -r)
+    width=$( echo $videomanifest | jq ".streams[$k].width" -r)
+    lurl=$( echo $videomanifest | jq ".streams[$k].url" -r)
+    ((lresolution=$height*$width))
+
+    if (($lresolution > $resolution)); then
+        if [[ "$lurl" != "" ]]; then
+            url=$(echo $lurl)
+            resolution=$(echo $lresolution)
+        fi
+    fi
+echo $lresolution - $url
+done
+# echo $url
 
 # for debug
 echo $url
