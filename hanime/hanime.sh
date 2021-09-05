f5aa9e1e49ce605fdca8d17a629cc983a75be957
Zhāng Jūn <60116198+idolmatster@users.noreply.github.com>
2021-09-05 04:31:59 +0200

    fixed major bug of not downloading page

--- hanime/hanime.sh
+++ hanime/hanime.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #download webpage
-#page=$(curl $1)
-page=$(cat index.html)
+page=$(curl $1)
+# page=$(cat index.html)
 
 # content at the beginnig of and end of script tag
 data=$(echo ${page##*window.__NUXT__=})
@@ -14,14 +14,15 @@ data2=$(echo $data | sed 's|\u002F|/|g')
 # for debug
 #echo $data2 > tmp.json
 
-# TODO : FIX choosing highest quality stream instead of 720p
 # parse the title and url with jq from script json of the page
 title=$(echo $data2 | jq '.state.data.video.hentai_video.name' -r)
 
 videomanifest=$(echo $data2 | jq '.state.data.video.videos_manifest.servers[0]' -r)
 
-((resolution=0))
+
+# iterate through array and pick best stream
 url="empty"
+((resolution=0))
 
 for k in  $(jq -r '.streams | keys | .[]' <<< "$videomanifest"); do
     height=$( echo $videomanifest | jq ".streams[$k].height" -r)
@@ -29,19 +30,23 @@ for k in  $(jq -r '.streams | keys | .[]' <<< "$videomanifest"); do
     lurl=$( echo $videomanifest | jq ".streams[$k].url" -r)
     ((lresolution=$height*$width))
 
+    # when higher res and there is a stream link set it as stream link
     if (($lresolution > $resolution)); then
         if [[ "$lurl" != "" ]]; then
             url=$(echo $lurl)
             resolution=$(echo $lresolution)
         fi
     fi
-echo $lresolution - $url
+
+    # for debug
+    # echo $lresolution - $url
+
 done
 # echo $url
 
 # for debug
-echo $url
-echo $title
+# echo $url
+# echo $title
 
 # downloading stream with ffmpeg
 ffmpeg -i "${url}" -bsf:a aac_adtstoasc -vcodec copy -c copy -crf 50 "${title}.mp4"
