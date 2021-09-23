a2451362a31ec6486ad47ffb3d09d1c09a76d732
Zhāng Jūn <60116198+idolmatster@users.noreply.github.com>
2021-09-23 15:20:31 +0200

    upgraded script
    - does not require page count anymore
    - does not create a temp file anymore

--- ehentai/run.sh
+++ ehentai/run.sh
@@ -1,44 +1,43 @@
 #/bin/bash
+link="$1"
 
 pagecount="$2"
-baseurl="$1"
+baseurl="$link"
 pageadd="?p="
 echo "$baseurl"
 
-tempfile="/tmp/urls$(date +%s).url"
 useragent="user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36 OPR/76.0.4017.177"
 
-get_gallery(){
-    curl "$galleryurl" \
-    -H 'authority: e-hentai.org' \
-    -H "$useragent"\
-    --compressed | pup 'div#gdt div.gdtm a attr{href}' > "$tempfile"
-}
+#preserver ifs
+SAVEIFS=$IFS   # Save current IFS
+IFS=$'\n'      # Change IFS to new line
 
-download_gallery(){
-    tempfile="$tempfile"
-    while IFS= read -r link
-        do
-            sleep 0.5
-            link=$(curl "$link" -H 'authority: e-hentai.org' -H "$useragent" --compressed | pup 'img#img attr{src}')
-            name=$(echo "$link" | rev | cut -d'/' -f 1 | rev)
-            curl "$link" -H "$useragent" -o "$name" -C -
-    done <"$tempfile"
+get_gallery(){
+    album_linksr=$(curl "$galleryurl" -H 'authority: e-hentai.org' -H "$useragent" --compressed | pup 'div#gdt div.gdtm a attr{href}')
+    album_linksa=($album_linksr)
+    echo "$album_linksa[@]"
+    echo "starting download content of page $I"
+    for link in "${album_linksa[@]}"
+    do
+        sleep 0.5
+        link=$(curl "$link" -H 'authority: e-hentai.org' -H "$useragent" --compressed | pup 'img#img attr{src}')
+        name=$(echo "$link" | rev | cut -d'/' -f 1 | rev)
+        echo "$name  - $link"
+        curl "$link" -H "$useragent" -o "$name" -C -
+    done
 }
 
 galleryname=$(curl "$baseurl" -H 'authority: e-hentai.org' -H "$useragent" --compressed | pup 'h1#gn text{}')
 mkdir "$galleryname"
 cd "$galleryname"
 
-echo $pagecount
-if [  -z "$2" ]
-    then
-        galleryurl="$baseurl"
-        get_gallery
-        download_gallery
+gallerycounter=$(curl "$link" -H 'authority: e-hentai.org' -H "$useragent" --compressed | pup 'table.ptt a text{}')
+pagecounter=($gallerycounter)
+pagecount=$(echo ${pagecounter[-2]})
 
-        rm "$tempfile"
-        exit 0
+echo $pagecount
+if [[ $pagecount == "" ]] ; then
+    pagecount=1
 fi
 
 for I in `seq 0 $pagecount`
@@ -46,7 +45,8 @@ do
     galleryurl="$baseurl$pageadd$I"
     echo "$galleryurl"
     get_gallery
-    download_gallery
+    # download_gallery
 done
 
-rm "$tempfile"
+# return to default ifs (let's hope this does not break other stuff but otherwise you know what to do!)
+IFS=$SAVEIFS 
