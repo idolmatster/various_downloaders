8b51e96abcf184e8ab0f1fc86b9bc4edd7aae84b
Zhāng Jūn <60116198+idolmatster@users.noreply.github.com>
2021-10-07 23:13:16 +0200

    Update cybash.sh

--- cyberdrop_bash/cybash.sh
+++ cyberdrop_bash/cybash.sh
@@ -1,53 +1,54 @@
 #!/bin/bash
-errordownloadpage(){
+
+cyerrordownloadpage(){
     echo "there was an error downloading the page try again later."
     echo "if the issue presists please report this as an issue on github"
-    exit
+    exit 1
 }
 
 #=====================================================
-#download page
+
+link="$1"
 
 echo "downloading page"
-webpage=$(wget $1 -q -O - || errordownloadpage )
+webpage=$(wget "$link" -q -O - || cyerrordownloadpage)
+# webpage=$(lynx -source "$link" || cyerrordownloadpage )
 
-echo "creating folder and preping for download"
-echo "if you wanna pause download crtl+c and rerun at a later point"
+if [[ "$webpage" == "" ]] ; then
+    echo "there was an error downloading the page try again later."
+    echo "if the issue presists please report this as an issue on github"
+    exit 1
+fi
 
 #extract important variables
 
-title=$(echo $webpage | pup 'h1#title' attr{title} | sed 's/\// - /g' | sed 's/:/ - /g' )
-
-# adjust for https or no https in link
-if [[ "${1}" == *"https://"* || "${1}" == *"http://"* ]]; then
-    id=$(echo $1 | cut -d '/' -f 5)
-else
-    id=$(echo $1 | cut -d '/' -f 3)
-fi
-
-filecountexpected=$(echo $webpage | pup 'p#totalFilesAmount' text{})
-
-album_linksr=$(echo $webpage | pup 'a.image' attr{href} )
-
-# creates the file list with ifs "splitting" on newline
-IFS=$'\n'      # Change IFS to new line
-SAVEIFS=$IFS   # Save current IFS
-album_linksa=($album_linksr)
-IFS=$SAVEIFS 
-
+title=$(echo "$webpage" | pup 'h1#title' attr{title} | sed 's/\// - /g' | sed 's/:/ - /g' )
 # echo $title 
+id=$(echo "${link#*cyberdrop.me}" | cut -d '/' -f 3)
 # echo $id 
+
+echo "creating folder \"$title ($id)\" and preping for download"
+echo "if you wanna pause download crtl+c and rerun at a later point"
 
 mkdir "$title ($id)"
 cd "$title ($id)"
+    
+filecountexpected=$(echo "$webpage" | pup 'p#totalFilesAmount' text{})
+
+album_linksr=$(echo "$webpage" | pup 'a.image' attr{href} )
+
+SAVEIFS="$IFS"   # Save current IFS
+IFS=$'\n'      # Change IFS to new line
+album_linksa=($album_linksr)
+IFS="$SAVEIFS"
 
 #====================================================
 
 echo "starting download"
-for link in "${album_linksa[@]}"
+for imgurl in "${album_linksa[@]}"
 do
-	wget -c "$link"
-    excho "$link"
+    wget -c -q --show-progress "$imgurl" || echo "error downloading $imgurl"
+    # echo "$imgurl"
 done
 
 #====================================================
@@ -56,19 +57,27 @@ filecountrecieved=$(ls | wc -l)
 echo "expected file count: $filecountexpected"
 echo "recieved file count: $filecountrecieved"
 if [[ $filecountrecieved -lt $filecountexpected ]] ; then
-
+    
     echo "re-downloading for verification"
-    for link in "${album_linksa[@]}"
+    for imgurl in "${album_linksa[@]}"
     do
-	    wget -c "$link"
+        wget -c -q --show-progress "$imgurl" || echo "error downloading $imgurl"
     done
     
+    filecountrecieved=$(ls | wc -l)
+
+
     if [[ $filecountrecieved -lt $filecountexpected ]]; then
+        echo "expected file count: $filecountexpected"
+        echo "recieved file count: $filecountrecieved"
         echo "there might be a temporary outage or a bug in the script"
         echo "please try again later if the error keep happening please report it"
+        echo "here is your link again "
+        echo "$link"
         exit 1
     fi
-    exit 0
+    echo "successfully downloaded the missing content"
+
 fi 
 echo "if there are any broken files the download might have broke while in progress"
 echo "in that case just run the script again"
