d9e52661152f47b40696388c3a7e75b15dc155bd
Zhāng Jūn <60116198+idolmatster@users.noreply.github.com>
2021-10-26 00:48:12 +0200

    Create tokenscript.go

---
+++ imgur/tokenscript.go
@@ -0,0 +1,31 @@
+package main
+  
+import (
+    "fmt"
+    "os"
+	"strings"
+	"time"
+	"strconv"
+	"io/ioutil"
+)
+  
+func main() {
+
+	content, err := ioutil.ReadFile("script.js")
+    if err != nil {
+		// exiting without further error in case of read error
+		os.Exit(1)
+    }
+
+    // Convert []byte to string and print to screen
+    text := string(content)
+
+	// cleverly sclice the script output file
+    split := strings.Split(text, "o=\"https://rt.\".concat(\"imgur.com\"),")
+	splita := split[1]
+	split = strings.Split(splita, "s=\"")
+	splita = split[1]
+	split = strings.Split(splita, "\"")
+	fmt.Println(split[0])
+	os.Exit(0)
+}
