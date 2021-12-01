f90a8490e2d2f453102cbd844667cbd683c074de
Zhāng Jūn <60116198+idolmatster@users.noreply.github.com>
2021-12-01 15:16:32 +0100

    no fix will be made for at least 3 months

--- cyberdrop/howtouse.js
+++
@@ -1,6 +0,0 @@
-const cyberdrop = require('./cyberdrop');
-main()
-async function main() {
-    console.log(await cyberdrop.getGallery('https://cyberdrop.me/a/cktntNj1'));
-    cyberdrop.downloadGallery('https://cyberdrop.me/a/cktntNj1');
-}
