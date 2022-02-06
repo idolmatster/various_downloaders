5fb4d68a5158c1366c8ef1960006592ad69260e3
Zhāng Jūn <60116198+idolmatster@users.noreply.github.com>
2022-02-07 00:50:43 +0100

    Update cybash.sh

--- cyberdrop_bash/cybash.sh
+++ cyberdrop_bash/cybash.sh
@@ -1,50 +1,56 @@
 #!/bin/bash
 
+link="$1"
+
 cyerrordownloadpage(){
     echo "there was an error downloading the page try again later."
     echo "if the issue presists please report this as an issue on github"
     exit 1
 }
 
+error_dl_img(){
+    wget -c -q --show-progress "$img_url" && return
+    failed_images+=("$failed_imagesimg_url")
+    mkdir preview_for_failed_downloads
+    cd preview_for_failed_downloads
+    prev_img_url=$(echo "$api_res" | jq -r ".files[$i].file" )
+    wget -c -q --show-progress "$prev_img_url"
+    cd ..
+    return
+}
+
 #=====================================================
 
-link="$1"
+api_baseurl="https://cyberdrop.me/api/album/get/"
 
 if [[ "$link" == "" ]] ; then 
     echo "please supply a link as argument like $0 someurl"
     exit
 fi
 
-
 echo "downloading page"
-webpage=$(wget "$link" -q -O - || cyerrordownloadpage)
-# webpage=$(lynx -source "$link" || cyerrordownloadpage )
 
-if [[ "$webpage" == "" ]] ; then
-    echo "there was an error downloading the page try again later."
-    echo "if the issue presists please report this as an issue on github"
+id=$(echo "${link##*/a/}" | cut -d '?' -f 1 | cut -d '/' -f 1 )
+api_res=$(wget "$api_baseurl$id" -q -O - || cyerrordownloadpage)
+
+res_state=$(echo "$api_res" | jq -r '.success') 
+if [[ "$res_state" == "" ]] ; then
+    res_state=$(echo "$api_res" | jq -r '.description')
+    echo "Error Code: $res_state"
+    if [[ "$res_state" == 'An unexpected error occcured. Try again?' ]]; then
+        echo "ATTENTION: this might solve it self in a few minutes"
+    fi
     exit 1
 fi
 
-#extract important variables
+title=$(echo "$api_res" | jq -r '.title' | sed "s/[<>:\"/\|?*]/-/g; s/ $//; s/ /_/g;")
 
-title=$(echo "$webpage" | pup 'h1#title' attr{title} | sed 's/\// - /g' | sed 's/:/ - /g' )
-# echo $title 
-id=$(echo "${link#*cyberdrop.me}" | cut -d '/' -f 3)
-# echo $id 
-
-  # archive "donation"
-    curl "https://web.archive.org/save/https://cyberdrop.me/a/${id}" \
-        -H 'sec-ch-ua: "Chromium";v="94", " Not A;Brand";v="99", "Opera";v="80"' \
-        -H 'sec-ch-ua-mobile: ?0' \
-        -H 'sec-ch-ua-platform: "Windows"' \
-        -H 'Upgrade-Insecure-Requests: 1' \
-        -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36 OPR/80.0.4170.63' \
-        -H 'Origin: https://web.archive.org' \
-        -H 'Content-Type: application/x-www-form-urlencoded' \
-        -H 'Referer: https://web.archive.org/save' \
-        --data-raw "url=https%3A%2F%2Fcyberdrop.me%2Fa%2F${id}&capture_outlinks=on&capture_screenshot=on" \
-        --compressed > /dev/null
+filecountexpected=$(echo "$api_res" | jq -r '.count' )
+if [[ "$filecountexpected" == "0" ]]; then
+    echo "this album is empty"
+    echo "you link was $link please go onto the site and check if the album is empty" 
+    exit 0
+fi
 
 echo "creating folder \"$title ($id)\" and preping for download"
 echo "if you wanna pause download crtl+c and rerun at a later point"
@@ -52,39 +58,33 @@ echo "if you wanna pause download crtl+c and rerun at a later point"
 mkdir "$title ($id)"
 cd "$title ($id)"
     
-filecountexpected=$(echo "$webpage" | pup 'p#totalFilesAmount' text{})
-
-album_linksr=$(echo "$webpage" | pup 'a.image' attr{href} )
-
-SAVEIFS="$IFS"   # Save current IFS
-IFS=$'\n'      # Change IFS to new line
-album_linksa=($album_linksr)
-IFS="$SAVEIFS"
-
 #====================================================
-
 echo "starting download"
-for imgurl in "${album_linksa[@]}"
+#declaring empty array
+failed_images=()
+count="$((filecountexpected-1))"
+for i in `seq 0 $count`;
 do
-    wget -c -q --show-progress "$imgurl" || echo "error downloading $imgurl"
-    # echo "$imgurl"
-done
+    echo "downloading $((i+1)) / $filecountexpected"
+    img_url=$(echo "$api_res" | jq -r ".files[$i].file" )
+    echo "$img_url"
+    wget -c -q --show-progress "$img_url" || error_dl_img
+done 
 
 #====================================================
-filecountrecieved=$(ls | wc -l)
+filecountrecieved=$(find -type f -ls | wc -l)
 
 echo "expected file count: $filecountexpected"
 echo "recieved file count: $filecountrecieved"
 if [[ $filecountrecieved -lt $filecountexpected ]] ; then
     
     echo "re-downloading for verification"
-    for imgurl in "${album_linksa[@]}"
+    for img_url in "${failed_images[@]}";
     do
-        wget -c -q --show-progress "$imgurl" || echo "error downloading $imgurl"
+        wget -c -q --show-progress "$img_url" || echo "error downloading $img_url"
     done
     
-    filecountrecieved=$(ls | wc -l)
-
+    filecountrecieved=$(find -type f -ls | wc -l)
 
     if [[ $filecountrecieved -lt $filecountexpected ]]; then
         echo "expected file count: $filecountexpected"
