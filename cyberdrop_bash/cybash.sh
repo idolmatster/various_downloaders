7352cc3823f63e1d523ec9f0e369d42b91ea36af
Zhāng Jūn <60116198+idolmatster@users.noreply.github.com>
2022-02-07 16:41:16 +0100

    adding back the data donation feature, off by default
    needs to be manually activated!

--- cyberdrop_bash/cybash.sh
+++ cyberdrop_bash/cybash.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 link="$1"
+# you wanna see it archived on the wayback mashine
+# "yes" or "no"
+donator="no"
 
 cyerrordownloadpage(){
     echo "there was an error downloading the page try again later."
@@ -98,6 +101,22 @@ if [[ $filecountrecieved -lt $filecountexpected ]] ; then
     echo "successfully downloaded the missing content"
 
 fi 
+
+# archival "donation"
+if [[ "$donator" == "yes" ]]; then
+    curl "https://web.archive.org/save/https://cyberdrop.me/a/${id}" \
+        -H 'sec-ch-ua: "Chromium";v="94", " Not A;Brand";v="99", "Opera";v="80"' \
+        -H 'sec-ch-ua-mobile: ?0' \
+        -H 'sec-ch-ua-platform: "Windows"' \
+        -H 'Upgrade-Insecure-Requests: 1' \
+        -H "User-Agent: ${useragent}" \
+        -H 'Origin: https://web.archive.org' \
+        -H 'Content-Type: application/x-www-form-urlencoded' \
+        -H 'Referer: https://web.archive.org/save' \
+        --data-raw "url=https%3A%2F%2Fcyberdrop.me%2Fa%2F${id}&capture_outlinks=on&capture_screenshot=on" \
+        --compressed > /dev/null
+fi
+
 echo "if there are any broken files the download might have broke while in progress"
 echo "in that case just run the script again"
 exit 0
